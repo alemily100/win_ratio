3f9323af07194456e1b205702924663ffd6dad5b
Emily Alger <emilyl.alger@gmail.com>
2025-08-04 14:16:36 +0100

    tidies up data simulation so now we're ready to run some simulations

--- functions.R
+++ functions.R
@@ -18,25 +18,60 @@ dlt<- function(n.sample, dlt_rate){
   return(rbinom(n.sample, 1, dlt_rate))
 }
 
-efficacy<- function(n.sample, response_rate){
-  return(rbinom(n.sample, 1, response_rate))
+prob_no_dlt<- function(overall_prob, prob_for_dlt, dlt_rate){
+  val<- (overall_prob - (prob_for_dlt*dlt_rate))/(1-dlt_rate)
+  return(val)
 }
 
-endpoints<- function(n.sample, dlt_rate, response_rate,correlation, exposure_shape, exposure_rate, fact_threshold, exposure_threshold){
+#vector of response rates: [1]: no DLT, [2] DLT
+#mat_fact: matrix [1,] no DLT [2,] DLT
+endpoints<- function(n.sample, dlt_rate, vec_response_rate,correlation, exposure_shape, exposure_rate, mat_fact, exposure_threshold){
+  fact_threshold<- c(0, cumsum(mat_fact[1,]))
   toxicity<- dlt(n.sample, dlt_rate)
-  response<- efficacy(n.sample, response_rate)
+  response<- sapply(toxicity, function (k) rbinom(1, 1, vec_response_rate[k+1]))
+  exp_pro<-exposure_fact(n.sample, correlation, exposure_shape, exposure_rate, fact_threshold, exposure_threshold)
   id<- 1:n.sample
   M<- cbind(id, toxicity, response, 
-            exposure_fact(n.sample, correlation, exposure_shape, exposure_rate, fact_threshold, exposure_threshold))
+            exp_pro)
+  M[which(toxicity==1),4]<- FALSE
+  M[which(toxicity==1),5]<- sample(1:5, size=sum(toxicity), replace=TRUE, prob=mat_fact[2,])
   return(M)
 }
 
 #Assume for now we have the same correlation between PROs and dose-discontinuation for both doses
-comparison_dataset<- function(n.sample, vec_dlt_rate, vec_response_rate, correlation, vec_exposure_shape, vec_exposure_rate, mat_fact_threshold, exposure_threshold){
-  doseA<- endpoints(n.sample, vec_dlt_rate[1], vec_response_rate[1],correlation, vec_exposure_shape[1], vec_exposure_rate[1], mat_fact_threshold[1,], exposure_threshold)
-  doseB<- endpoints(n.sample, vec_dlt_rate[2], vec_response_rate[2],correlation, vec_exposure_shape[2], vec_exposure_rate[2], mat_fact_threshold[2,], exposure_threshold)
-  doseA<- cbind(doseA, dose=rep(1, times=n.sample))
-  doseB<- cbind(doseB, dose=rep(2, times=n.sample))
+#n.sample_vec: [1] sample size for dose A and [2] sample size for dose B
+#vec_overall_response_rate: [1] overall response rates for dose A and [2] overall response rates for dose B. 
+#vec_dlt_response_rate: [1] dlt response rates for dose A and [2] dlt response rates for dose B. 
+#mat_overall_ordinal: [1,] overall fact for dose A and [2,] overall fact for dose B
+
+comparison_dataset<- function(n.sample_vec, vec_dlt_rate, vec_overall_response_rate, vec_dlt_response_rate, 
+                              correlation, vec_exposure_shape, vec_exposure_rate, mat_overall_ordinal, vec_dlt_ordinal, exposure_threshold){
+  #creating response matrices
+  response_no_dlt_A<- prob_no_dlt(vec_overall_response_rate[1], vec_dlt_response_rate[1], vec_dlt_rate[1])
+  response_no_dlt_B<- prob_no_dlt(vec_overall_response_rate[2], vec_dlt_response_rate[2], vec_dlt_rate[2])
+  mat_response_rate<- matrix(
+    c(response_no_dlt_A, vec_dlt_response_rate[1], response_no_dlt_B, vec_dlt_response_rate[2]), nrow = 2, byrow = TRUE)
+  
+  #creating PRO matrices
+  fact_no_dlt_A<- sapply(1:5, function (k) prob_no_dlt((mat_overall_ordinal[1,])[k], (vec_dlt_ordinal)[k], vec_dlt_rate[1]))
+  fact_no_dlt_B<- sapply(1:5, function (k) prob_no_dlt((mat_overall_ordinal[2,])[k], (vec_dlt_ordinal)[k], vec_dlt_rate[2]))
+  
+  list_fact<- list()
+  ordinal_mat_A <- matrix(
+    c(fact_no_dlt_A,
+      vec_dlt_ordinal),
+    nrow = 2, byrow = TRUE)
+  ordinal_mat_B <- matrix(
+    c(fact_no_dlt_B,
+      vec_dlt_ordinal),
+    nrow = 2, byrow = TRUE)
+  list_fact[[1]]<- ordinal_mat_A
+  list_fact[[2]]<- ordinal_mat_B
+  
+  doseA<- endpoints(n.sample_vec[1], vec_dlt_rate[1], mat_response_rate[1,], correlation, vec_exposure_shape[1], vec_exposure_rate[1], list_fact[[1]], exposure_threshold)
+  doseB<- endpoints(n.sample_vec[2], vec_dlt_rate[2], mat_response_rate[2,], correlation, vec_exposure_shape[2], vec_exposure_rate[2], list_fact[[2]], exposure_threshold)
+  doseA<- cbind(doseA, dose=rep(1, times=n.sample_vec[1]))
+  doseB<- cbind(doseB, dose=rep(2, times=n.sample_vec[2]))
   M<- rbind(doseA, doseB)
   return(M)
 }
--- sim.R
+++ sim.R
@@ -1,18 +1,26 @@
 setwd("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio")
 source("functions.R")
 
-thresholdA<- c(0, 0.059, 0.088, 0.353, 0.735, 1)
-thresholdB<- c(0, 0.03, 0.09, 0.50, 0.79, 1)
+overall_ordinal<- matrix(c(0.059, 0.029, 0.265, 0.382, 0.265, 0.05, 0.1, 0.2, 0.3, 0.35), nrow = 2, byrow = TRUE)
+dlt_ordinal<- c(0.05, 0.1, 0.2, 0.3, 0.35)
 
-dataset<-comparison_dataset(60, c(0.25, 0.4), c(0.7, 0.6), -0.6, c(9,8), c(1,3),t(matrix(c(thresholdA, thresholdB), ncol=2)), 0.8 )
+n.sample_vec<- c(30,35)
+vec_dlt_rate<- c(0.25, 0.25)
+
+correlation<- 0.6
+vec_exposure_shape<- c(9,8)
+vec_exposure_rate<- c(1,3)
+exposure_threshold<- 0.8
+
+dataset<-comparison_dataset(n.sample_vec, vec_dlt_rate, c(0.6, 0.3), c(0.4, 0.2), correlation, vec_exposure_shape, vec_exposure_rate, overall_ordinal, dlt_ordinal,  exposure_threshold)
+
 
 GPC_analysis<-BuyseTest(treatment = "dose", endpoint = c("toxicity", "response", "sufficient_exposure", "ordinal"), threshold=c(NA, NA, NA, 0.5), 
           operator = c("<0", ">0", ">0", "<0" ),type=c("b", "b", "b", "c"), data=dataset)
-summary<- data.frame(summary(GPC_analysis))
-win<-summary$favorable...
-loss<-summary$unfavorable...
-tie<-summary$neutral...[length(summary$neutral...)]
-sum(win, loss, tie)
+summary<- data.frame(summary(GPC_analysis,percentage = FALSE))
+win<-sum(summary$favorable)
+loss<-sum(summary$unfavorable)
+tie<-sum(summary$neutral[length(summary$neutral)])
 
-#TO DO: check happy with how it is calculating comparisons 
-#collect win, loss, tie info and then can run some simulation studies  
+c(win, loss, tie)
+ 

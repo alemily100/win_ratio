e772d1aa2480242745b23e23f98157947b444143
Emily Alger <emilyl.alger@gmail.com>
2025-08-05 14:58:36 +0100

    finalised with DLT added to copula

--- functions.R
+++ functions.R
@@ -2,7 +2,7 @@ library(MASS)
 library(tidyverse)
 library(BuyseTest)
 
-exposure_fact<- function(n.sample, correlation_pro_exp,correlation_pro_dlt ,exposure_shape, exposure_rate, fact_threshold, exposure_threshold, dlt_rate){
+dlt_exposure_fact<- function(n.sample, correlation_pro_exp,correlation_pro_dlt ,exposure_shape, exposure_rate, fact_threshold, exposure_threshold, dlt_rate){
   M<- matrix(nrow=n.sample, ncol=3)
   gauss<-mvrnorm(n.sample, rep(0, times=3), Sigma=matrix(c(1, correlation_pro_exp,0,correlation_pro_exp, 1,correlation_pro_dlt,0,correlation_pro_dlt,1), nrow=3))
   exposure<-qbeta(pnorm(gauss[,1]), exposure_shape, exposure_rate)
@@ -17,25 +17,21 @@ exposure_fact<- function(n.sample, correlation_pro_exp,correlation_pro_dlt ,expo
   return(M)
 }
 
-
-
 prob_no_dlt<- function(overall_prob, prob_for_dlt, dlt_rate){
   val<- (overall_prob - (prob_for_dlt*dlt_rate))/(1-dlt_rate)
   return(val)
 }
 
+
 #vector of response rates: [1]: no DLT, [2] DLT
 #mat_fact: matrix [1,] no DLT [2,] DLT
-endpoints<- function(n.sample, dlt_rate, vec_response_rate,correlation, exposure_shape, exposure_rate, mat_fact, exposure_threshold){
-  fact_threshold<- c(0, cumsum(mat_fact[1,]))
-  toxicity<- dlt(n.sample, dlt_rate)
-  response<- sapply(toxicity, function (k) rbinom(1, 1, vec_response_rate[k+1]))
-  exp_pro<-exposure_fact(n.sample, correlation, exposure_shape, exposure_rate, fact_threshold, exposure_threshold)
+endpoints<- function(n.sample, dlt_rate, vec_response_rate,correlation_pro_exp,correlation_pro_dlt, exposure_shape, exposure_rate, vec_fact, exposure_threshold){
+  fact_threshold<- c(0, cumsum(vec_fact))
+  M<-dlt_exposure_fact(n.sample,correlation_pro_exp,correlation_pro_dlt, exposure_shape, exposure_rate, fact_threshold, exposure_threshold, dlt_rate)
+  response<- sapply(M[,1], function (k) rbinom(1, 1, vec_response_rate[k+1]))
   id<- 1:n.sample
-  M<- cbind(id, toxicity, response, 
-            exp_pro)
-  M[which(toxicity==1),4]<- FALSE
-  M[which(toxicity==1),5]<- sample(1:5, size=sum(toxicity), replace=TRUE, prob=mat_fact[2,])
+  M<- cbind(id, M, response)
+  M[which(M[,1]==1),3]<- FALSE
   return(M)
 }
 
@@ -46,31 +42,15 @@ endpoints<- function(n.sample, dlt_rate, vec_response_rate,correlation, exposure
 #mat_overall_ordinal: [1,] overall fact for dose A and [2,] overall fact for dose B
 
 comparison_dataset<- function(n.sample_vec, vec_dlt_rate, vec_overall_response_rate, vec_dlt_response_rate, 
-                              correlation, vec_exposure_shape, vec_exposure_rate, mat_overall_ordinal, vec_dlt_ordinal, exposure_threshold){
+                              correlation_pro_exp, correlation_pro_dlt, vec_exposure_shape, vec_exposure_rate, mat_fact, exposure_threshold){
   #creating response matrices
   response_no_dlt_A<- prob_no_dlt(vec_overall_response_rate[1], vec_dlt_response_rate[1], vec_dlt_rate[1])
   response_no_dlt_B<- prob_no_dlt(vec_overall_response_rate[2], vec_dlt_response_rate[2], vec_dlt_rate[2])
   mat_response_rate<- matrix(
     c(response_no_dlt_A, vec_dlt_response_rate[1], response_no_dlt_B, vec_dlt_response_rate[2]), nrow = 2, byrow = TRUE)
   
-  #creating PRO matrices
-  fact_no_dlt_A<- sapply(1:5, function (k) prob_no_dlt((mat_overall_ordinal[1,])[k], (vec_dlt_ordinal)[k], vec_dlt_rate[1]))
-  fact_no_dlt_B<- sapply(1:5, function (k) prob_no_dlt((mat_overall_ordinal[2,])[k], (vec_dlt_ordinal)[k], vec_dlt_rate[2]))
-  
-  list_fact<- list()
-  ordinal_mat_A <- matrix(
-    c(fact_no_dlt_A,
-      vec_dlt_ordinal),
-    nrow = 2, byrow = TRUE)
-  ordinal_mat_B <- matrix(
-    c(fact_no_dlt_B,
-      vec_dlt_ordinal),
-    nrow = 2, byrow = TRUE)
-  list_fact[[1]]<- ordinal_mat_A
-  list_fact[[2]]<- ordinal_mat_B
-  
-  doseA<- endpoints(n.sample_vec[1], vec_dlt_rate[1], mat_response_rate[1,], correlation, vec_exposure_shape[1], vec_exposure_rate[1], list_fact[[1]], exposure_threshold)
-  doseB<- endpoints(n.sample_vec[2], vec_dlt_rate[2], mat_response_rate[2,], correlation, vec_exposure_shape[2], vec_exposure_rate[2], list_fact[[2]], exposure_threshold)
+  doseA<- endpoints(n.sample_vec[1], vec_dlt_rate[1], mat_response_rate[1,], correlation_pro_exp, correlation_pro_dlt, vec_exposure_shape[1], vec_exposure_rate[1], mat_fact[1,], exposure_threshold)
+  doseB<- endpoints(n.sample_vec[2], vec_dlt_rate[2], mat_response_rate[2,], correlation_pro_exp, correlation_pro_dlt,  vec_exposure_shape[2], vec_exposure_rate[2], mat_fact[2,], exposure_threshold)
   doseA<- cbind(doseA, dose=rep(1, times=n.sample_vec[1]))
   doseB<- cbind(doseB, dose=rep(2, times=n.sample_vec[2]))
   M<- rbind(doseA, doseB)
@@ -78,31 +58,30 @@ comparison_dataset<- function(n.sample_vec, vec_dlt_rate, vec_overall_response_r
 }
 
 analysis<- function(n.sample_vec, vec_dlt_rate, vec_overall_response_rate, vec_dlt_response_rate, 
-                    correlation, vec_exposure_shape, vec_exposure_rate, mat_overall_ordinal, vec_dlt_ordinal, exposure_threshold){
+                    correlation_pro_exp, correlation_pro_dlt, vec_exposure_shape, vec_exposure_rate, mat_fact, exposure_threshold){
   dataset<- comparison_dataset(n.sample_vec, vec_dlt_rate, vec_overall_response_rate, vec_dlt_response_rate, 
-                               correlation, vec_exposure_shape, vec_exposure_rate, mat_overall_ordinal, vec_dlt_ordinal, exposure_threshold)
-  GPC_analysis_4<-BuyseTest(treatment = "dose", endpoint = c("toxicity", "response", "sufficient_exposure", "ordinal"), threshold=c(NA, NA, NA, 0.1), 
+                               correlation_pro_exp, correlation_pro_dlt, vec_exposure_shape, vec_exposure_rate, mat_fact, exposure_threshold)
+  GPC_analysis_4<-BuyseTest(treatment = "dose", endpoint = c("dlt", "response", "sufficient_exposure", "ordinal"), threshold=c(NA, NA, NA, 0.1),
                           operator = c("<0", ">0", ">0", "<0" ),type=c("b", "b", "b", "c"), data=dataset)
   summary_4<- data.frame(summary(GPC_analysis_4,percentage = FALSE))
   win_4<-sum(summary_4$favorable)
   loss_4<-sum(summary_4$unfavorable)
   tie_4<-sum(summary_4$neutral[length(summary_4$neutral)])
-  
+
   dataset_utility<-data.frame(dataset)%>%
     mutate(utility = case_when(
-      toxicity == 1 & response == 0 ~ 0,
-      toxicity == 0 & response == 0 ~ 30,
-      toxicity == 1 & response == 1 ~ 50,
-      toxicity == 0 & response == 1 ~ 100
+      dlt == 1 & response == 0 ~ 0,
+      dlt == 0 & response == 0 ~ 30,
+      dlt == 1 & response == 1 ~ 50,
+      dlt == 0 & response == 1 ~ 100
     ))
-  
-  GPC_analysis_utility<-BuyseTest(treatment = "dose", endpoint = c("utility", "sufficient_exposure", "ordinal"), threshold=c(0.1, NA, 0.1), 
+
+  GPC_analysis_utility<-BuyseTest(treatment = "dose", endpoint = c("utility", "sufficient_exposure", "ordinal"), threshold=c(0.1, NA, 0.1),
                                   operator = c(">0", ">0", "<0" ),type=c("c", "b", "c"), data=dataset_utility)
-  
+
   summary_utility<- data.frame(summary(GPC_analysis_utility,percentage = FALSE))
   win_utility<-sum(summary_utility$favorable)
   loss_utility<-sum(summary_utility$unfavorable)
   tie_utility<-sum(summary_utility$neutral[length(summary_utility$neutral)])
-  
   return(list(four = c(win_4, loss_4, tie_4), utility = c(win_utility, loss_utility, tie_utility)))
 } 
--- sim.R
+++ sim.R
@@ -2,28 +2,23 @@ setwd("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial D
 source("functions.R")
 set.seed(11600)
 
-n.sim<-10
-
-overall_ordinal<- matrix(c(0.059, 0.029, 0.265, 0.382, 0.265, 0.05, 0.1, 0.2, 0.3, 0.35), nrow = 2, byrow = TRUE)
-dlt_ordinal<- c(0.05, 0.1, 0.2, 0.3, 0.35)
+n.sim<-1000
 
 n.sample_vec<- c(30,30)
-vec_dlt_rate<- c(0.25, 0.35)
-
-correlation<- 0.6
+vec_dlt_rate<- c(0.267, 0.417)
+overall_efficacy<- c(0.8, 0.875)
+dlt_efficacy<- c(0.1, 0.6)
+correlation_pro_exp<- -0.6
+correlation_pro_dlt<- 0.5
 vec_exposure_shape<- c(9,9)
-vec_exposure_rate<- c(1,1.5)
-
-#probability not sufficient intensity
-pbeta(0.8, 9, 1)
-pbeta(0.8, 9, 1.5)
-
+vec_exposure_rate<- c(1,2)
+mat_fact<- matrix(c(0.059, 0.029, 0.265, 0.382, 0.265, 0.02, 0.03, 0.10, 0.45, 0.4), nrow=2, byrow=TRUE) 
 exposure_threshold<- 0.8
-overall_efficacy<-c(0.6, 0.6)
-dlt_efficacy<- c(0.4, 0.4)
+
 
 test<-replicate(n.sim,
-  analysis(n.sample_vec, vec_dlt_rate, overall_efficacy, dlt_efficacy , correlation, vec_exposure_shape, vec_exposure_rate, overall_ordinal, dlt_ordinal,  exposure_threshold),
+  analysis(n.sample_vec, vec_dlt_rate, overall_efficacy, dlt_efficacy , correlation_pro_exp, correlation_pro_dlt,
+           vec_exposure_shape, vec_exposure_rate, mat_fact,  exposure_threshold),
   simplify = FALSE)
 
 four_endpoints <- do.call(rbind, lapply(test, `[[`, "four"))
@@ -32,5 +27,5 @@ utility_endpoints <- do.call(rbind, lapply(test, `[[`, "utility"))
 wr_four<-sapply(1:n.sim, function (k) four_endpoints[k,1]/four_endpoints[k,2])
 wr_utility<-sapply(1:n.sim, function (k) utility_endpoints[k,1]/utility_endpoints[k,2])
 
-hist(wr_four, freq=FALSE)
+hist(wr_four, freq=FALSE, xlim=c(0,4))
 lines(density(wr_utility))

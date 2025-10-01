970bf14eed3f5b72be8593f027f9f6f6814eb305
Emily Alger <emilyl.alger@gmail.com>
2025-10-01 10:57:00 +0100

    swap around ties and response columns

--- additional_analysis.R
+++ additional_analysis.R
@@ -7,8 +7,8 @@ response_priority_scenario1 <- read.csv("C:/Users/ealger/OneDrive - The Institut
 tie_priority_scenario1 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario1/wins_tie_priority_scenario1.csv")[,-1]
 
 sc1<-rbind(c("DLT", colMeans(dlt_priority_scenario1)),
-c("response", colMeans(response_priority_scenario1)),
-c("ties",colMeans(tie_priority_scenario1)))
+c("ties", colMeans(tie_priority_scenario1)),
+c("response",colMeans(response_priority_scenario1)))
 
 #scenario2
 dlt_priority_scenario2 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario2/wins_dlt_priority_scenario2.csv")[,-1]
@@ -16,8 +16,8 @@ response_priority_scenario2 <- read.csv("C:/Users/ealger/OneDrive - The Institut
 tie_priority_scenario2 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario2/wins_tie_priority_scenario2.csv")[,-1]
 
 sc2<-rbind(c("DLT", colMeans(dlt_priority_scenario2)),
-           c("response", colMeans(response_priority_scenario2)),
-           c("ties",colMeans(tie_priority_scenario2)))
+           c("ties", colMeans(tie_priority_scenario2)),
+           c("response",colMeans(response_priority_scenario2)))
 
 #scenario3
 dlt_priority_scenario3 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario3/wins_dlt_priority_scenario3.csv")[,-1]
@@ -25,8 +25,8 @@ response_priority_scenario3 <- read.csv("C:/Users/ealger/OneDrive - The Institut
 tie_priority_scenario3 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario3/wins_tie_priority_scenario3.csv")[,-1]
 
 sc3<-rbind(c("DLT", colMeans(dlt_priority_scenario3)),
-           c("response", colMeans(response_priority_scenario3)),
-           c("ties",colMeans(tie_priority_scenario3)))
+           c("ties", colMeans(tie_priority_scenario3)),
+           c("response",colMeans(response_priority_scenario3)))
 
 #scenario4
 dlt_priority_scenario4 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario4/wins_dlt_priority_scenario4.csv")[,-1]
@@ -34,8 +34,8 @@ response_priority_scenario4 <- read.csv("C:/Users/ealger/OneDrive - The Institut
 tie_priority_scenario4 <- read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/win_ratio/sim_results/scenario4/wins_tie_priority_scenario4.csv")[,-1]
 
 sc4<-rbind(c("DLT", colMeans(dlt_priority_scenario4)),
-           c("response", colMeans(response_priority_scenario4)),
-           c("ties",colMeans(tie_priority_scenario4)))
+           c("ties", colMeans(tie_priority_scenario4)),
+           c("response",colMeans(response_priority_scenario4)))
 
 
-write.csv(rbind(sc1, sc2, sc3, sc4), "mean_winlosstie.csv"
+write.csv(rbind(sc1, sc2, sc3, sc4), "mean_winlosstie.csv")
